cdac423563d16387338892f58fbcedb7acbdbf65
Salmon Joy <salmonjoy1996@gmail.com>
2025-03-18 13:57:48 +0530

    github action testing automation

---
+++ calculator.js
@@ -0,0 +1,7 @@
+// A simple calculator module
+const calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+  };
+  
+  module.exports = calculator;
---
+++ calculator.test.js
@@ -0,0 +1,17 @@
+const calculator = require('./calculator');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(calculator.add(1, 2)).toBe(3);
+});
+
+test('adds 1 + -2 to equal -1', () => {
+    expect(calculator.add(1, -2)).toBe(-1);
+  });
+
+test('subtract 2 from 5 equals 3', () => {
+  expect(calculator.subtract(5, 2)).toBe(3);
+});
+
+test('subtract -2 from -5 equals -3', () => {
+    expect(calculator.subtract(-5, -2)).toBe(-3);
+  });
